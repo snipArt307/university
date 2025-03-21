14684ad14bc63a99a20b98414921076784c97f79
snipArt307 <artem.semkin.04@mail.ru>
2025-03-21 09:29:05 +0700

    commit1

--- main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-int main(int argc, char *argv[]) {
-    execl("/home/artem/c_prog_university/hello", "hello", "0", (char *) NULL);
-    perror("execl failed");
-    exit(1);
-}
