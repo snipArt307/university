c750153970d02b73466d8ab50a6ee022d301e8f3
snipArt307 <artem.semkin.04@mail.ru>
2025-03-21 13:23:52 +0700

    commit2

--- ResourceManagement/lab_3/hello.c
+++ ResourceManagement/lab_3/hello.c
@@ -4,20 +4,25 @@
 #include <sys/wait.h>
 
 int main(int argc, char *argv[]) {
-	int level = atoi(argv[1]); 
-	printf("The process %d staring working on level %d.\n", getpid(), level);
-	if (level == 5) {
+	int level = atoi(argv[1]);
+	int index = atoi(argv[2]);
+	printf("The process %d staring working on level %d.\n", getpid(), index);
+
+	if (level == 0) {
 		printf("end\n");
 		exit(0);
 	} else {
 		pid_t pid1 = fork();
-		level++;
+		index++;
+		level--;
 		
 		char level_str[12];
+		char index_str[12];
+		sprintf(index_str, "%d", index);
                 sprintf(level_str, "%d", level);
                 
 		if (pid1 == 0) {
-			execl("/home/artem/c_prog_university/hello", "hello", level_str, (char *) NULL);
+			execl("./hello", "hello", level_str, index_str, (char *) NULL);
 			perror("execl failed");
                         exit(1);
 		}
@@ -25,7 +30,7 @@ int main(int argc, char *argv[]) {
 		pid_t pid2 = fork();
 		
 		if (pid2 == 0) {
-			execl("/home/artem/c_prog_university/hello", "hello", level_str, (char *) NULL);
+			execl("./hello", "hello", level_str, index_str,  (char *) NULL);
 			perror("execl failed");
                         exit(1);
 		}
--- ResourceManagement/lab_3/main.c
+++ ResourceManagement/lab_3/main.c
@@ -3,7 +3,20 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-    execl("/home/artem/c_prog_university/hello", "hello", "0", (char *) NULL);
-    perror("execl failed");
-    exit(1);
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <level>\n", argv[0]);
+		exit(1);
+	}
+
+	int level = atoi(argv[1]);
+
+	if (level > 5 || level <= 0) {
+		fprintf(stderr, "Input error\n");
+		exit(1);
+	}
+	
+	char *level_str = argv[1];
+        execl("./hello", "hello", level_str, "0", (char *) NULL);
+        perror("execl failed");
+        exit(1);
 }
